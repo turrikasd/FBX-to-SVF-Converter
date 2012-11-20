a66555544dde06ba86afd36cce0c0fcd02644156
Despite <despite.mn@gmail.com>
2012-11-21 01:23:17 +0200

    Saving to .svf working

--- FBX to SVF Converter/Program.cs
+++ FBX to SVF Converter/Program.cs
@@ -43,8 +43,17 @@ namespace FBX_to_SVF_Converter
 
         static bool BeginConversion(string path)
         {
-            string file;
+            string file, fileName;
             int FBXVersion;
+
+            // Get the file name
+            int p = path.LastIndexOf("\\");
+            if (p != -1)
+                fileName = path.Substring(p + 1);
+            else
+                fileName = path;
+
+            fileName = fileName.Substring(0, fileName.Length - 4);
 
             // Read the file to a string
             StreamReader sw = new StreamReader(path);
@@ -61,10 +70,10 @@ namespace FBX_to_SVF_Converter
             switch (FBXVersion)
             {
                 case 7300:
-                    Convert7300(file);
+                    Convert7300(file, fileName);
                     break;
                 case 6100:
-                    Convert6100(file);
+                    Convert6100(file, fileName);
                     break;
                 default:
                     Console.WriteLine("Non-Supported FBXVersion. Please contact the distributor or upgrade your modeling software.");
@@ -74,11 +83,12 @@ namespace FBX_to_SVF_Converter
             return true;
         }
 
-        static bool Convert7300(string file)
+        static bool Convert7300(string file, string fileName)
         {
             int vertexCount, indexCount;
             string[] vertices, indices;
 
+            // Find vertex count
             int a = file.IndexOf("Vertices:");
             int b = file.Substring(a).IndexOf(' ');
             int c = file.Substring(a + b + 2).IndexOf('{');
@@ -87,10 +97,11 @@ namespace FBX_to_SVF_Converter
 
             Console.WriteLine("Model contains " + vertexCount + " vertices");
 
+            // Find vertices
             int d = file.Substring(a + b + c).IndexOf("a: ") + a + b + c;
             int e = file.Substring(d).IndexOf("\n");
 
-            vertices = file.Substring(d + 3, e).Split(',');
+            vertices = file.Substring(d + 3, e - 3).Split(',');
 
             for (int i = 0; i < vertices.Count(); i++)
             {
@@ -100,11 +111,50 @@ namespace FBX_to_SVF_Converter
                 Console.WriteLine(vertices[i]);
             }
 
+            // Print a space between vertices and indices
+            Console.WriteLine();
+
+            // Find index count
+            int f = file.IndexOf("PolygonVertexIndex:");
+            int g = file.Substring(f).IndexOf(' ');
+            int h = file.Substring(f + g + 2).IndexOf('{');
+            if (!int.TryParse(file.Substring(a + b + 2, c - 1), out indexCount)) return false;
+
+            Console.WriteLine("Model contains " + indexCount + " indices");
+
+            // Find indices
+            int j = file.Substring(f + g + h).IndexOf("a: ") + f + g + h;
+            int k = file.Substring(j).IndexOf("\n");
+
+            indices = file.Substring(j + 3, k - 3).Split(',');
+
+            foreach (string s in indices)
+            {
+                Console.Write(s + ", ");
+            }
+
+            SaveToFile(vertexCount, indexCount, vertices, indices, fileName);
+
             return true;
         }
 
-        static bool Convert6100(string file)
+        static bool Convert6100(string file, string fileName)
         {
+            return true;
+        }
+
+        static bool SaveToFile(int vertexCount, int indexCount, string[] vertices, string[] indices, string fileName)
+        {
+            if (!Directory.Exists("SVF"))
+                Directory.CreateDirectory("SVF");
+
+            StreamWriter sw = new StreamWriter("SVF\\" + fileName + ".svf");
+
+            sw.WriteLine("Vertices: " + vertexCount);
+            sw.WriteLine("Indices: " + indexCount);
+
+            sw.Close();
+
             return true;
         }
     }
